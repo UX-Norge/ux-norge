e2c48bde4d6f0ab2c3854fb7d970592da9570adf
Tobias Wulvik <tobias@umble.no>
2022-05-12 20:26:38 +0200

    Slack Banner added to sanity

--- web/src/features/coverPage/components/SlackBanner.tsx
+++ web/src/features/coverPage/components/SlackBanner.tsx
@@ -1,11 +1,24 @@
 import { VectorIllustrations } from "@Images/VectorIllustrations";
+import { SlackBanner as SlackBannerType } from "@Types";
 import { Button } from "@Ui/Button";
 import { Body1, Heading4 } from "@Ui/Typography";
+import { graphql, useStaticQuery } from "gatsby";
 import * as React from "react";
 
 interface IProps {}
 
 export const SlackBanner: React.FC<IProps> = ({}) => {
+  const {
+    sanitySlackBanner: { title, text, buttonText },
+  } = useStaticQuery<{ sanitySlackBanner: SlackBannerType }>(graphql`
+    query {
+      sanitySlackBanner {
+        title
+        text
+        buttonText
+      }
+    }
+  `);
   return (
     <div className="bg-yellow-100 px-48 pt-8">
       <div className="mx-auto flex w-full max-w-page items-center justify-between">
@@ -15,12 +28,12 @@ export const SlackBanner: React.FC<IProps> = ({}) => {
             className="w-48"
           />
           <div>
-            <Heading4>Bli med 3071 medlemmer i UX Norge-slacken</Heading4>
-            <Body1>Diskuter alt fra Norman doors til innsiktsmetoder</Body1>
+            <Heading4>{title}</Heading4>
+            <Body1>{text}</Body1>
           </div>
         </div>
         <Button href="https://uxnorge.slack.com/join/shared_invite/zt-n4kqavud-8ZYV08oyR7WuVukZkSR69Q#/shared-invite/email">
-          Bli med
+          {buttonText}
         </Button>
       </div>
     </div>
