f733e2e06a27d720174c63862ca274c1bb9cb232
Tobias Wulvik <tobias@umble.no>
2022-05-20 13:28:43 +0200

    adding slack webhook

--- web/src/api/slackMessage.ts
+++ web/src/api/slackMessage.ts
@@ -10,5 +10,17 @@ export default function handler(
 ) {
   console.log(req.body);
 
+  fetch(
+    "https://hooks.slack.com/services/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",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: {
+        text: "Test!",
+      },
+    }
+  );
   res.status(200).json({ hello: `world` });
 }
