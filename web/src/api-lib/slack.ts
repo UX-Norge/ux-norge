42247958893fd76353e9946db8d9f379cbaf793d
Tobias Wulvik <tobias@umble.no>
2022-05-23 17:23:54 +0200

    slack send message test #2

---
+++ web/src/api-lib/slack.ts
@@ -0,0 +1,32 @@
+// Require the Node Slack SDK package (github.com/slackapi/node-slack-sdk)
+import { WebClient, LogLevel, ChatPostMessageArguments } from "@slack/web-api";
+
+console.log(process.env.SLACK_TOKEN);
+// WebClient instantiates a client that can call API methods
+// When using Bolt, you can use either `app.client` or the `client` passed to listeners.
+const client = new WebClient(process.env.SLACK_TOKEN, {
+  // LogLevel can be imported and used to make debugging simpler
+  logLevel: LogLevel.DEBUG,
+});
+
+export async function publishMessage(
+  channelId: string,
+  text: string,
+  blocks: ChatPostMessageArguments["blocks"]
+) {
+  try {
+    // Call the chat.postMessage method using the built-in WebClient
+    const result = await client.chat.postMessage({
+      // The token you used to initialize your app
+      token: process.env.SLACK_TOKEN,
+      channel: channelId,
+      blocks,
+      // You could also use a blocks[] array to send richer content
+    });
+
+    // Print result, which includes information about the message (like TS)
+    console.log(result);
+  } catch (error) {
+    console.error(error);
+  }
+}
--- web/src/api/slack-message.ts
+++ web/src/api/slack-message.ts
@@ -1,12 +1,13 @@
 import { GatsbyFunctionRequest, GatsbyFunctionResponse } from "gatsby";
 import { isValidRequest } from "@sanity/webhook";
 import fetch from "node-fetch";
+import { publishMessage } from "../api-lib/slack";
 
 interface SlackMessageBody {
   text: string;
 }
 
-export default function handler(
+export default async function handler(
   req: GatsbyFunctionRequest,
   res: GatsbyFunctionResponse
 ) {
@@ -18,14 +19,50 @@ export default function handler(
     return;
   }
 
-  fetch(slackWebhook, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
+  const { title, description, link } = {
+    title: "Senior UX-designer",
+    description:
+      "Gjør det mest meningsfulle designarbeidet i din karriere. Bli med å lage plattformen som skaper morgendagens velferdssamfunn.",
+    link: "https://uxnorge.no/stillignsannonse/senior-ux-designer3/",
+  };
+
+  console.log(req.body);
+
+  const channelId = process.env.SLACK_ADS_CHANNEL_ID as string;
+  publishMessage(channelId, "Test", [
+    {
+      type: "section",
+      text: {
+        type: "plain_text",
+        text: "[Umble — Oslo / Remote]",
+        emoji: true,
+      },
     },
-    body: {
-      text: "Test!",
+    {
+      type: "header",
+      text: {
+        type: "plain_text",
+        text: title,
+        emoji: true,
+      },
     },
-  });
-  res.status(200).json({ hello: `world` });
+    {
+      type: "section",
+      text: {
+        type: "plain_text",
+        text: description,
+      },
+    },
+    {
+      type: "divider",
+    },
+    {
+      type: "section",
+      text: {
+        type: "mrkdwn",
+        text: link,
+      },
+    },
+  ]);
+  res.status(200).json({});
 }
