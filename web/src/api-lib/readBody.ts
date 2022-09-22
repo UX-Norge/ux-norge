bea24031ba3ea742105578a0fa32edd69abec787
Tobias Wulvik <tobias.wulvik@gmail.com>
2022-09-22 13:55:02 +0200

    updated sanity webhook

---
+++ web/src/api-lib/readBody.ts
@@ -0,0 +1,7 @@
+export async function readBody(readable: any) {
+  const chunks = [];
+  for await (const chunk of readable) {
+    chunks.push(typeof chunk === "string" ? Buffer.from(chunk) : chunk);
+  }
+  return Buffer.concat(chunks).toString("utf8");
+}
