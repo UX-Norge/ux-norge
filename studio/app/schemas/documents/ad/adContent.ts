e5c5a2b8e798019d6307945ca4bc1135d228e989
Tobias Wulvik <tobias.wulvik@gmail.com>
2023-01-06 12:27:32 +0100

    article image and youtube in ad

---
+++ studio/app/schemas/documents/ad/adContent.ts
@@ -0,0 +1,46 @@
+export default {
+  title: "Block Content",
+  name: "adContent",
+  type: "array",
+  of: [
+    {
+      title: "Block",
+      type: "block",
+      styles: [
+        { title: "Normal", value: "normal" },
+        { title: "Tittel", value: "h2" },
+        { title: "Undertittel", value: "h3" },
+        { title: "Quote", value: "blockquote" },
+      ],
+      lists: [
+        { title: "Bullet", value: "bullet" },
+        { title: "Numbered", value: "number" },
+      ],
+      marks: {
+        decorators: [
+          { title: "Strong", value: "strong" },
+          { title: "Emphasis", value: "em" },
+        ],
+        annotations: [
+          {
+            title: "URL",
+            name: "link",
+            type: "object",
+            fields: [
+              {
+                title: "URL",
+                name: "href",
+                type: "url",
+              },
+            ],
+          },
+        ],
+      },
+    },
+    {
+      type: "articleImage",
+      render: "ArticleImageRenderer",
+    },
+    { type: "youtube" },
+  ],
+};
