f61d1a898320d42e1ec66bb36eda01f5ad689584
Aswin <aswin@umble.no>
2023-01-16 10:11:05 +0100

    sok

---
+++ web/src/pages/sok.tsx
@@ -0,0 +1,60 @@
+import { ArticleGrid } from "@Components/ArticleGrid";
+import { Article, GraphqlEdges } from "@Types";
+import { PageWrapper } from "@Ui/Layout";
+import { GetServerData } from "gatsby";
+import * as React from "react";
+import { sanityClient } from "../api-lib/sanity.client";
+
+interface DataProps {
+  allSanityAd: GraphqlEdges;
+}
+
+export const JobPage = ({
+  serverData: { searchHits },
+}: {
+  serverData: { searchHits: Article[] };
+}) => {
+  return (
+    <PageWrapper>
+      <div className="mx-auto max-w-page p-24">
+        <ArticleGrid articles={searchHits} />
+      </div>
+    </PageWrapper>
+  );
+};
+
+export const getServerData: GetServerData<{ searchHits: Article[] }> = async ({
+  query,
+}) => {
+  const searchHits = await sanityClient.fetch(
+    `*[_type == "article" && _score > 0 && !(_id in path("drafts.**"))]
+    | score(
+      boost(title match $searchTerm, 100),
+      boost(author.name match "*" + $searchTerm + "*", 20),
+      boost(title match "*" + $searchTerm + "*", 10),
+      boost(description match $searchTerm, 1)
+    )
+    | order(_score desc) 
+    {
+      title,
+      description,
+      mainImage,
+      slug{
+        current
+      }
+    }`,
+    {
+      searchTerm: query && query.searchTerm,
+    }
+  );
+
+  console.log(searchHits);
+
+  return {
+    props: {
+      searchHits,
+    },
+  };
+};
+
+export default JobPage;
