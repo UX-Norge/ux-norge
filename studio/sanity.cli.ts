c4edc044585f7139daf5bf09bd47740d122ebd61
Einar Stubhaug <einar@ravn.no>
2023-12-04 10:34:46 +0100

    rikige verdier i sanity.cli.ts

--- studio/sanity.cli.ts
+++ studio/sanity.cli.ts
@@ -2,7 +2,7 @@ import { defineCliConfig } from 'sanity/cli';
 
 export default defineCliConfig({
   api: {
-    projectId: '1zkk2ha0',
-    dataset: 'production',
+    projectId: 'dcdo4kbx',
+    dataset: 'staging',
   },
 });
