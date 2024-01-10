f7a80e3e2d488781384c2cd1d31410b92ed10039
Einar Stubhaug <einar@ravn.no>
2024-01-10 09:52:42 +0100

    Sanity instansierer klienten med ny syntaks etter oppdatering

--- web/src/api-lib/sanity.client.ts
+++ web/src/api-lib/sanity.client.ts
@@ -1,6 +1,5 @@
-const client = require("@sanity/client");
-
-export const sanityClient = client({
+import {createClient} from '@sanity/client'
+export const sanityClient = createClient({
   projectId: process.env.SANITY_PROJECT_ID,
   dataset: process.env.SANITY_DATASET,
   apiVersion: "2022-05-23", // use current UTC date - see "specifying API version"!
