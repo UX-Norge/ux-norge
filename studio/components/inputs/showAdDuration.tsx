925b90ab9967fc69c46f3eafd9f846b90fe007af
Terje Syversen <terje@ravn.no>
2024-12-17 13:39:16 +0100

    Validering og utregning av varighet på annonser

--- studio/components/inputs/showAdDuration.tsx
+++ studio/components/inputs/showAdDuration.tsx
@@ -2,30 +2,35 @@ import React, { useEffect, useState } from "react";
 import { useClient, useFormValue } from "sanity";
 
 const printDate = (startDate: string, duration = 0) => {
+  if (!startDate) {
+    return null;
+  }
   const date = new Date(startDate);
   date.setDate(date.getDate() + duration);
   return date.toLocaleDateString("no-NO", { dateStyle: "long" });
 };
 
 const ShowAdDuration = React.forwardRef(() => {
-  const packageTypeRef = useFormValue(["packageType", "_ref"]) as number;
-  const createdAt = useFormValue(["_createdAt"]) as string;
-  const client = useClient({ apiVersion: "2021-03-25" });
-
-  const [duration, setDuration] = useState(0);
-
-  useEffect(() => {
-    packageTypeRef &&
-      client
-        .fetch(`*[_type == 'adPackageType' && _id == $ref][0]`, {
-          ref: packageTypeRef,
-        })
-        .then((result: { duration: number }) => setDuration(result.duration));
-  }, [client]);
-
-  if (!packageTypeRef) return null;
-
-  return <div>Varer til: {printDate(createdAt, duration)}</div>;
+  const startDate = useFormValue(["startDate"]) as string;
+  if (startDate) {
+    const packageTypeRef = useFormValue(["packageType", "_ref"]) as number;
+    const client = useClient({ apiVersion: "2021-03-25" });
+    const [duration, setDuration] = useState(0);
+  
+    useEffect(() => {
+      packageTypeRef &&
+        client
+          .fetch(`*[_type == 'adPackageType' && _id == $ref][0]`, {
+            ref: packageTypeRef,
+          })
+          .then((result: { duration: number }) => setDuration(result.duration));
+    }, [client]);
+    
+    if (packageTypeRef) {
+      return <div>Varer til: {printDate(startDate, duration)}</div>;
+    }  
+  }
+  return null
 });
 
 export default ShowAdDuration;
