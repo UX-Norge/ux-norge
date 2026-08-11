117948c0327aeb2808a9e22b483ab59e85e1ea20
mats@ravn.no <mats@ravn.no>
2026-08-11 12:33:46 +0200

    fjernet Partnerside-fane og feltbeskrivelser

--- studio/schemas/documents/company.ts
+++ studio/schemas/documents/company.ts
@@ -27,16 +27,11 @@ export default {
   name: "company",
   title: "Company",
   type: "document",
-  groups: [
-    { name: "company", title: "Bedrift", default: true },
-    { name: "partnerPage", title: "Partnerside" },
-  ],
   fields: [
     {
       name: "name",
       title: "Navn*",
       type: "string",
-      group: "company",
       validation: (Rule: any) => Rule.required(),
     },
     {
@@ -44,13 +39,11 @@ export default {
       title: "Logo",
       type: "image",
       description: "Helst .svg",
-      group: "company",
     },
     {
       name: "code",
       title: "Firmakode",
       type: "string",
-      group: "company",
       components: {
         input: PasswordGenerator,
       },
@@ -58,22 +51,17 @@ export default {
     {
       name: "isPartner",
       title: "Er støttespiller",
-      description: "Vises i støttespiller-listen på forsiden",
       type: "boolean",
-      group: "company",
     },
     {
       name: "hasPartnerPage",
       title: "Har partnerside",
-      description: "Oppretter en egen side på /partner/{slug}/",
       type: "boolean",
-      group: "partnerPage",
     },
     {
       name: "slug",
       title: "Slug",
       type: "slug",
-      group: "partnerPage",
       options: {
         source: "name",
         maxLength: 96,
@@ -91,35 +79,30 @@ export default {
       name: "partnerTitle",
       title: "Tittel",
       type: "string",
-      group: "partnerPage",
       hidden: onlyWhenNoPartnerPage,
     },
     {
       name: "partnerMainImage",
       title: "Hovedbilde",
       type: "articleImage",
-      group: "partnerPage",
       hidden: onlyWhenNoPartnerPage,
     },
     {
       name: "partnerDescription",
       title: "Ingress",
       type: "text",
-      group: "partnerPage",
       hidden: onlyWhenNoPartnerPage,
     },
     {
       name: "partnerBody",
       title: "Brødtekst",
       type: "articleContent",
-      group: "partnerPage",
       hidden: onlyWhenNoPartnerPage,
     },
     {
       name: "companyArticles",
       title: "Bedriftens artikler",
       type: "string",
-      group: "partnerPage",
       readOnly: true,
       components: {
         input: CompanyArticlesInput,
